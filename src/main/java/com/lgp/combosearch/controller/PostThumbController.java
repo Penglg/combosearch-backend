121f52167f7c17c68de451abd0553cbc27933e90
your-user-name <your-email-address-on-gitee>
2024-03-27 21:25:23 +0800

    style(*): 重命名目录结构

---
+++ src/main/java/com/lgp/combosearch/controller/PostThumbController.java
@@ -0,0 +1,54 @@
+package com.lgp.combosearch.controller;
+
+import com.lgp.combosearch.common.BaseResponse;
+import com.lgp.combosearch.common.ErrorCode;
+import com.lgp.combosearch.common.ResultUtils;
+import com.lgp.combosearch.exception.BusinessException;
+import com.lgp.combosearch.model.dto.postthumb.PostThumbAddRequest;
+import com.lgp.combosearch.model.entity.User;
+import com.lgp.combosearch.service.PostThumbService;
+import com.lgp.combosearch.service.UserService;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子点赞接口
+ *
+ */
+@RestController
+@RequestMapping("/post_thumb")
+@Slf4j
+public class PostThumbController {
+
+    @Resource
+    private PostThumbService postThumbService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 点赞 / 取消点赞
+     *
+     * @param postThumbAddRequest
+     * @param request
+     * @return resultNum 本次点赞变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doThumb(@RequestBody PostThumbAddRequest postThumbAddRequest,
+            HttpServletRequest request) {
+        if (postThumbAddRequest == null || postThumbAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postThumbAddRequest.getPostId();
+        int result = postThumbService.doPostThumb(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+}
---
+++ src/main/java/com/lgp/combosearch/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.lgp.combosearch.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lgp.combosearch.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
